9b37b360c19ebdd5e2c98585a1d6a8d792fc4dd7
vaibha987 <v2goyal95@gmail.com>
2024-09-01 18:26:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your code here
+let source;
+
+document.querySelectorAll('.image').forEach(item => {
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragover', dragOver);
+  item.addEventListener('drop', dragDrop);
+});
+
+function dragStart(e) {
+  source = e.target;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop(e) {
+  let temp = e.target.style.backgroundImage;
+  e.target.style.backgroundImage = source.style.backgroundImage;
+  source.style.backgroundImage = temp;
+}
