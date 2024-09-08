3c69a6f7bee48b14d10d9f6578466c42b256f8a2
vaibha987 <v2goyal95@gmail.com>
2024-09-08 20:32:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
-let source;
-document.querySelectorAll('.image').forEach(item => {
-  item.addEventListener('dragstart', dragStart);
-  item.addEventListener('dragover', dragOver);
-  item.addEventListener('drop', dragDrop);
-});
+let dragindex = 0;
 
-function dragStart(e) {
- source = e.target;
-}
-function dragOver(e) {
-  e.preventDefault();
+let dropindex = 0;
+
+let clone = “”;
+
+const images = document.querySelectorAll(“.image”);
+
+function drag(e) {
+
+e.dataTransfer.setData(“text”, e.target.id);
+
 }
 
-function dragDrop(e) {
-  let temp = e.target.style.backgroundImage;
-  e.target.style.backgroundImage = source.style.backgroundImage;
-  source.style.backgroundImage = temp;
+function allowDrop(e) {
+
+e.preventDefault();
+
 }
+
+function drop(e) {
+
+clone = e.target.cloneNode(true);
+
+let data = e.dataTransfer.getData(“text”);
+
+let nodelist = document.getElementById(“parent”).childNodes;
+
+console.log(data, e.target.id);
+
+for (let i = 0; i < nodelist.length; i++) {
+
+if (nodelist[i].id == data) {
+
+  dragindex = i;
+
+}
+}
+
+dragdrop(clone);
+
+document
+
+.getElementById("parent")
+
+.replaceChild(document.getElementById(data), e.target);
+document
+
+.getElementById("parent")
+
+.insertBefore(
+
+  clone,
+
+  document.getElementById("parent").childNodes[dragindex]
+
+);
+}
+
+const dragdrop = (image) => {
+
+image.ondragstart = drag;
+
+image.ondragover = allowDrop;
+
+image.ondrop = drop;
+
+};
+
+images.forEach(dragdrop);
